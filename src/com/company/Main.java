ee331f1637f76bf01b04b9e5ee1ecdc80be37256
Sean <github.vitaexmachina.dev>
2022-06-22 15:50:01 +0100

    answers in comments main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,19 +5,19 @@ public class Main {
     public static void main(String[] args) {
 
 //        Coding Exercise 1: Speed Converter
-//        SpeedConverter.printConversion(1.5);
-//        SpeedConverter.printConversion(10.25);
-//        SpeedConverter.printConversion(-1);
-//        SpeedConverter.printConversion(16);
-//        SpeedConverter.printConversion(47);
+//        SpeedConverter.printConversion(1.5); //1.5 km/h = 1 mi/h
+//        SpeedConverter.printConversion(10.25); //10.25 km/h = 6 mi/h
+//        SpeedConverter.printConversion(-1); //Invalid Value
+//        SpeedConverter.printConversion(16); //16.0 km/h = 10 mi/h
+//        SpeedConverter.printConversion(47); //47.0 km/h = 29 mi/h
 
 //        Coding Exercise 2: MegaBytes Converter
-//        MegaBytesConverter.printMegaBytesAndKiloBytes(2500);
+//        MegaBytesConverter.printMegaBytesAndKiloBytes(2500); //2500 KB = 2 MB and 452 KB
 
 //        Coding Exercise 3: Barking Dog
-        System.out.println(BarkingDog.shouldWakeUp (true, 1)); // true
-        System.out.println(BarkingDog.shouldWakeUp (false, 2)); // false
-        System.out.println(BarkingDog.shouldWakeUp (true, 8)); // false
-        System.out.println(BarkingDog.shouldWakeUp (true, -1)); // false
+//        System.out.println(BarkingDog.shouldWakeUp (true, 1)); // true
+//        System.out.println(BarkingDog.shouldWakeUp (false, 2)); // false
+//        System.out.println(BarkingDog.shouldWakeUp (true, 8)); // false
+//        System.out.println(BarkingDog.shouldWakeUp (true, -1)); // false
     }
 }
